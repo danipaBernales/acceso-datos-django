251582fe617d2613395f0ba2e3460397fb2afee0
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2024-06-11 21:16:21 -0400

    Update admin.py

--- Sprint-Individual/kanbany/tareas/admin.py
+++ Sprint-Individual/kanbany/tareas/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Tarea, Etiqueta, Prioridad
 
 # Register your models here.
+admin.site.register(Tarea)
+admin.site.register(Etiqueta)
+admin.site.register(Prioridad)
