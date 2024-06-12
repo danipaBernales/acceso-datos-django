57601b73940b60c0cd299673571ffbef3536d39e
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2024-06-11 21:17:38 -0400

    Update forms.py

--- Sprint-Individual/kanbany/tareas/forms.py
+++ Sprint-Individual/kanbany/tareas/forms.py
@@ -1,7 +1,14 @@
 from django import forms
-from .models import Tarea
+from .models import Tarea, Etiqueta, Prioridad
 
 class TareaForm(forms.ModelForm):
     class Meta:
         model = Tarea
-        fields = ('titulo', 'descripcion', 'fecha_vencimiento', 'estado', 'etiqueta', 'prioridad')
+        fields = ('titulo', 'descripcion', 'fecha_vencimiento', 'estado', 'etiqueta', 'prioridad', 'usuario_asignado')
+         widgets = {
+            'fecha_vencimiento': forms.DateTimeInput(attrs={'type': 'datetime-local'}),
+            'estado': forms.Select(choices=Tarea.ESTADO_CHOICES),
+            'etiqueta': forms.Select(),
+            'prioridad': forms.Select(),
+            'usuario_asignado': forms.Select(),
+        }
