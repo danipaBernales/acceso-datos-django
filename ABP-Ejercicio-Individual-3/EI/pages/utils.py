1eb49c8175c1d8a772fd3b30c7733e00c9c3c8f6
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2024-06-27 22:23:44 -0400

    pages update

---
+++ ABP-Ejercicio-Individual-3/EI/pages/utils.py
@@ -0,0 +1,14 @@
+from tasks.models import Task
+from django.contrib.auth import get_user_model
+def check_user(user):
+    return user.username if str(user)!="AnonymousUser" else False
+def check_tasks(user):
+    if not check_user(user):
+        return False
+    else:
+        user_object=get_user_model().objects.get(username=user.username)
+        try:
+            task=Task.objects.filter(owner=user_object).order_by("-expire_date")
+            return task
+        except Task.DoesNotExist:
+            return False
