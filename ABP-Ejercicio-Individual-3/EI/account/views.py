8866f1c598d8994743d15e17bec8fa8187f42413
Daniela Paz Bernales <dbernales.miranda@gmail.com>
2024-06-27 22:22:12 -0400

    Workflow update

---
+++ ABP-Ejercicio-Individual-3/EI/account/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from account.decorators.requestsDecorator import page_in_construction
+from django.contrib.auth import logout
+from django.shortcuts import redirect
+# Create your views here.
+
+@page_in_construction
+def test(requests):
+    return HttpResponse("<h1>ESTAS AQUI</h1>")
+
+def logout_view(request):
+    logout(request)
+    return redirect('/')
+@page_in_construction
+def profile(requests):
+    pass
